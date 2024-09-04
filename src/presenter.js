8cf1c86dbd3d66515d169240dbf262fb7225cb0d
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-09-04 19:38:10 -0400

    Muestra la pocicion Inical del carrito

--- src/presenter.js
+++ src/presenter.js
@@ -1,12 +1,15 @@
-
+import PocicionInciial from "./optenerPocicionInicial";
 const Comandos = document.querySelector("#Comandos");
 const form = document.querySelector("#superficie-form");
 const div = document.querySelector("#PocicionInicial-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const ValorComandos = first.value;
   
-  div.innerHTML = "<p>"+ + "</p>";
+  const ValorComandos = first.value;
+  PosXY=PocicionInciial(ValorComandos);
+  PosInicialX=Number(PosXY[0]);
+  PosInicialY=Number(PosXY[1]);
+  Direccion=PosXY[2];
+  div.innerHTML = "<p>La Pocicion Inicial Es: " + PosInicialX + "," + PosInicialY + " Direccion: " + Direccion  + "</p>";
 });
