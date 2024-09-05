5cb65916e702f6bcf487abf059496848bf1a9f21
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-09-04 21:45:15 -0400

    funcion para que el auto gire a la derecha

--- src/controlarMovimiento.js
+++ src/controlarMovimiento.js
@@ -1,12 +1,62 @@
+function girar(direccion,comando)
+{
+    switch(direccion)
+    {
+        case "N":
+        {
+            if(comando==="D")
+            {
+                return "E";
+            }
+            
+            break;
+        }
+        case "S":
+        {
+            if(comando==="D")
+            {
+                return "O";
+            }
+            
+
+            break;
+        }
+        case "E":
+        {
+            if(comando==="D")
+            {
+                return "S";
+            }
+            
+
+            break;
+        }
+        case "O":
+        {
+            if(comando==="D")
+            {
+                return "N";
+            }
+            
+            break;
+        }
+    }
+}
 function MobAuto(tamX,tamY,posX,posY,Direccion,Comandos)
 {
     if(Direccion==="N" || Direccion==="S" || Direccion==="E" || Direccion==="O")
     {
-        
+        for(let i=0;i<Comandos.length;i++)
+        {
+            if(Comandos[i]==="D")
+            {
+                Direccion=girar(Direccion,comando[i]);
+            }
+        }
     }
     else
     {
-        return "direccion incorrecta";
+        return "direccion Incorrecta";
     }
 
 }
