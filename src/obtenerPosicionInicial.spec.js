6491d99bd4aa06115b044296fdb47bcd2fe2a10a
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-09-04 21:44:27 -0400

    Se agrego mas pruebas para obtener poscion inicial

--- src/obtenerPosicionInicial.spec.js
+++ src/obtenerPosicionInicial.spec.js
@@ -1,7 +1,13 @@
 import PosicionInicial from "./obtenerPosicionInicial";
 
 describe("Posicion Inicial",()=>{
-    it("Deberia devolver La posicion Inicial de la cadena ingresada",()=>{
+    it("Deberia devolver La posicion Inicial de la cadena ingresada 1,1,O",()=>{
         expect(PosicionInicial("6,6/1,1O/AIADAIA")).toEqual(["1","1","O"]);
+    });
+    it("Deberia devolver La posicion Inicial de la cadena ingresada 3,3,E",()=>{
+        expect(PosicionInicial("5,5/3,3E/AADAADADDA")).toEqual(["3","3","E"]);
+    });
+    it("Deberia devolver La posicion Inicial de la cadena ingresada 1,2,N",()=>{
+        expect(PosicionInicial("5,5/1,2N/IAIAIAIAA")).toEqual(["1","2","N"]);
     })
 })
