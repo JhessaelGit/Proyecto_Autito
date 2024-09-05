4aeb7daf12887a1c612681f635c52b538f418300
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-09-04 21:49:52 -0400

    Se agrego prueba para ver los comandos

--- src/obtenerComandos.spec.js
+++ src/obtenerComandos.spec.js
@@ -1,7 +1,13 @@
 import ObtenerComandos from "./obtenerComandos";
 
 describe("Comandos",()=>{
-    it("Deberia devolver Los Comandos de la cadena ingresada",()=>{
+    it("Deberia devolver Los Comandos de la cadena ingresada AIADAIA ",()=>{
         expect(ObtenerComandos("6,6/1,1O/AIADAIA")).toEqual(["A","I","A","D","A","I","A"]);
+    });
+    it("Deberia devolver Los Comandos de la cadena ingresada IAIAIAIAA",()=>{
+        expect(ObtenerComandos("5,5/1,2N/IAIAIAIAA")).toEqual(["I","A","I","A","I","A","I","A","A"]);
+    });
+    it("Deberia devolver Los Comandos de la cadena ingresada AADAADADDA",()=>{
+        expect(ObtenerComandos("5,5/3,3E/AADAADADDA")).toEqual(["A","A","D","A","A","D","A","D","D","A"]);
     })
 })
