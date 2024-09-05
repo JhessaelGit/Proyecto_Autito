92b0c21cdec4deec0ff8951b7d9a6c7e015ecba1
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-09-04 23:08:34 -0400

    se agrego la funcionalidad de avanzar y las pruebas correspondientes

--- src/controlarMovimiento.js
+++ src/controlarMovimiento.js
@@ -54,9 +54,48 @@ function girar(direccion,comando)
         }
     }
 }
+function Avanzar(Direccion,posX,posY,tamX,tamY)
+{
+    let salidaXY=[posX,posY];
+    switch(Direccion)
+    {
+        case "N":
+        {
+            salidaXY[1]=posY+1;
+            break;
+        }
+        case "S":
+        {
+            salidaXY[1]=posY-1;
 
+            break;
+        }
+        case "E":
+        {
+            salidaXY[0]=posX+1;
+
+            break;
+        }
+        case "O":
+        {
+            salidaXY[0]=posX-1;
+
+            break;
+        }
+
+    }
+    if(salidaXY[0]<=tamX && salidaXY[0]>=0 && salidaXY[1]<=tamY && salidaXY[1]>=0)
+    {
+        return salidaXY;
+    }
+    else
+    {
+        return ["Fuera de rango",""];
+    }
+}
 function MobAuto(tamX,tamY,posX,posY,Direccion,Comandos)
 {
+    let salidaXY=[];
     if(Direccion==="N" || Direccion==="S" || Direccion==="E" || Direccion==="O")
     {
         for(let i=0;i<Comandos.length;i++)
@@ -69,13 +108,28 @@ function MobAuto(tamX,tamY,posX,posY,Direccion,Comandos)
             {
                 Direccion=girar(Direccion,Comandos[i]);
             }
+            else if(Comandos[i]==="A")
+            {
+                salidaXY=Avanzar(Direccion,posX,posY,tamX,tamY)
+                console.log("2XY: " + salidaXY +"  X: "+ posX + "  Y: "+ posY + " i: " + i + " Dir: " + Direccion);
+
+                if(salidaXY[0]==="Fuera de rango")
+                {
+                    return "El Autito Salio Fuera Del Rango";
+                }
+                posX=salidaXY[0];
+                posY=salidaXY[1];
+            }
+            console.log("XY: " + salidaXY +"  X: "+ posX + "  Y: "+ posY + " i: " + i + " Dir: " + Direccion);
+
         }
     }
     else
     {
         return "direccion Incorrecta";
     }
-
+    salidaXY[2]=Direccion;
+    return salidaXY;
 }
 
 export default MobAuto;
--- src/controlarMovimiento.spec.js
+++ src/controlarMovimiento.spec.js
@@ -1,7 +1,13 @@
 import MobAuto from "./controlarMovimiento";
-
-describe("Movimiento del auto", () => {
-  it("deberia obtener el movimiento del auto", () => {
-    expect(MobAuto(6,6,2,1,"F","AIADAIA")).toEqual("direccion incorrecta");
+describe("Tamanio Superficie", () => {
+  it("deberia Obtener la posicion final del auto que se salio de rango", () => {
+    expect(MobAuto(6,6,1,1,"O","AIADAIA")).toEqual("El Autito Salio Fuera Del Rango");
   });
+  it("deberia Obtener la posicion final del auto en 1,3N", () => {
+    expect(MobAuto(5,5,1,2,"N","IAIAIAIAA")).toEqual([1,3,"N"]);
+  });
+  it("deberia Obtener la posicion final del auto 5,1N", () => {
+    expect(MobAuto(5,5,3,3,"E","AADAADADDA")).toEqual([5,1,"E"]);
+  });
+  
 });
