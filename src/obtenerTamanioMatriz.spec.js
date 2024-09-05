e007285532e5491c521eb4d7ca962c151942ca3a
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-09-04 22:03:51 -0400

    se agregaron mas pruebas para obtener la superficie y se corrigio la funcion para obtener el tamanio

--- src/obtenerTamanioMatriz.spec.js
+++ src/obtenerTamanioMatriz.spec.js
@@ -1,7 +1,13 @@
 import TamMatriz from "./obtenerTamanioMatriz";
 
 describe("Tamanio Superficie", () => {
-  it("deberia Obtener El tamanio de la superfice donde estara el autito", () => {
+  it("deberia Obtener El tamanio de la superfice donde estara el autito 6,6", () => {
     expect(TamMatriz("6,6/1,1O/AIADAIA")).toEqual(["6","6"]);
   });
+  it("deberia Obtener El tamanio de la superfice donde estara el autito de la primera superficie 5,5", () => {
+    expect(TamMatriz("5,5/3,3E/AADAADADDA")).toEqual(["5","5"]);
+  });
+  it("deberia Obtener El tamanio de la superfice donde estara el autito de la segunda superficie 5,5", () => {
+    expect(TamMatriz("5,5/1,2N/IAIAIAIAA")).toEqual(["5","5"]);
+  })
 });
